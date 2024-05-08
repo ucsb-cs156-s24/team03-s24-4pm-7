51c525640b27759dbeb865f69cb4009a91f7bf0f
Michelle Zimmermann <michellezimmermann@Michelles-MacBook-Pro.local>
2024-05-08 16:48:15 -0700

    creating articles form and stories

---
+++ frontend/src/main/components/Articles/ArticlesForm.js
@@ -0,0 +1,155 @@
+import { Button, Form } from 'react-bootstrap';
+import { useForm } from 'react-hook-form'
+import { useNavigate } from 'react-router-dom';
+
+function ArticlesForm({ initialContents, submitAction, buttonLabel = "Create" }) {
+
+    
+    // Stryker disable all
+    const {
+        register,
+        formState: { errors },
+        handleSubmit,
+    } = useForm(
+        { defaultValues: initialContents || {}, } 
+    );
+    // Stryker restore all
+   
+    const navigate = useNavigate();
+
+    const isodate_regex = /(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+)|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d)|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d)/i;
+
+    const testIdPrefix = "ArticlesForm";
+
+    return (
+        <Form onSubmit={handleSubmit(submitAction)}>
+
+            
+            {initialContents && (
+                <Form.Group className="mb-3" >
+                    <Form.Label htmlFor="id">Id</Form.Label>
+                    <Form.Control
+                        data-testid={testIdPrefix + "-id"}
+                        id="id"
+                        type="text"
+                        {...register("id")}
+                        value={initialContents.id}
+                        disabled
+                    />
+                </Form.Group>
+            )}
+
+            <Form.Group className="mb-3" >
+                <Form.Label htmlFor="title">Title</Form.Label>
+                <Form.Control
+                    data-testid={testIdPrefix + "-title"}
+                    id="title"
+                    type="text"
+                    isInvalid={Boolean(errors.name)}
+                    {...register("title", {
+                        required: "Title is required.",
+                        maxLength : {
+                            value: 30,
+                            message: "Max length 30 characters"
+                        }
+                    })}
+                />
+                <Form.Control.Feedback type="invalid">
+                    {errors.name?.message}
+                </Form.Control.Feedback>
+            </Form.Group>
+
+            <Form.Group className="mb-3" >
+                <Form.Label htmlFor="url">URL</Form.Label>
+                <Form.Control
+                    data-testid={testIdPrefix + "-url"}
+                    id="url"
+                    type="text"
+                    isInvalid={Boolean(errors.name)}
+                    {...register("url", {
+                        required: "URL is required.",
+                        maxLength : {
+                            value: 30,
+                            message: "Max length 30 characters"
+                        }
+                    })}
+                />
+                <Form.Control.Feedback type="invalid">
+                    {errors.name?.message}
+                </Form.Control.Feedback>
+            </Form.Group>
+
+            <Form.Group className="mb-3" >
+                <Form.Label htmlFor="explanation">Explanation</Form.Label>
+                <Form.Control
+                    data-testid={testIdPrefix + "-explanation"}
+                    id="explanation"
+                    type="text"
+                    isInvalid={Boolean(errors.name)}
+                    {...register("explanation", {
+                        required: "Explanation is required.",
+                        maxLength : {
+                            value: 100,
+                            message: "Max length 30 characters"
+                        }
+                    })}
+                />
+                <Form.Control.Feedback type="invalid">
+                    {errors.name?.message}
+                </Form.Control.Feedback>
+            </Form.Group>
+
+            <Form.Group className="mb-3" >
+                <Form.Label htmlFor="email">Email</Form.Label>
+                <Form.Control
+                    data-testid={testIdPrefix + "-email"}
+                    id="email"
+                    type="text"
+                    isInvalid={Boolean(errors.name)}
+                    {...register("email", {
+                        required: "Email is required.",
+                        maxLength : {
+                            value: 30,
+                            message: "Max length 30 characters"
+                        }
+                    })}
+                />
+                <Form.Control.Feedback type="invalid">
+                    {errors.name?.message}
+                </Form.Control.Feedback>
+            </Form.Group>
+
+            <Form.Group className="mb-3" >
+                <Form.Label htmlFor="localDateTime">Date (iso format)</Form.Label>
+                <Form.Control
+                    data-testid={testIdPrefix + "-localDateTime"}
+                    id="localDateTime"
+                    type="datetime-local"
+                    isInvalid={Boolean(errors.localDateTime)}
+                    {...register("localDateTime", { required: true, pattern: isodate_regex })}
+                />
+                <Form.Control.Feedback type="invalid">
+                    {errors.localDateTime && 'LocalDateTime is required. '}
+                </Form.Control.Feedback>
+            </Form.Group>
+
+            <Button
+                type="submit"
+                data-testid={testIdPrefix + "-submit"}
+            >
+                {buttonLabel}
+            </Button>
+            <Button
+                variant="Secondary"
+                onClick={() => navigate(-1)}
+                data-testid={testIdPrefix + "-cancel"}
+            >
+                Cancel
+            </Button>
+
+        </Form>
+
+    )
+}
+
+export default ArticlesForm;
