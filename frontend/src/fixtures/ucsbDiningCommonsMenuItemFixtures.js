becf6eafd04adc9813af75bdc3dcab108ffc95f6
chendashi <chendashi111@outlook.com>
2024-05-08 16:34:01 -0700

    zc - add diningCommons fixtures

---
+++ frontend/src/fixtures/ucsbDiningCommonsMenuItemFixtures.js
@@ -0,0 +1,31 @@
+const ucsbDiningCommonsMenuItemFixtures = {
+    oneItem: {
+        "id": 1,
+        "diningCommonsCode": "ortega",
+        "name": "Sesame Beef with Broccoli",
+        "station": "Entree Specials"
+    },
+    threeItems: [
+        {
+            "id": 1,
+            "diningCommonsCode": "ortega",
+            "name": "Kung Pao Chicken",
+            "station": "Entree Specials"
+        },
+        {
+            "id": 2,
+            "diningCommonsCode": "ortega",
+            "name": "Classic Miso Soup",
+            "station": "Entree"
+        },
+        {
+            "id": 3,
+            "diningCommonsCode": "ortega",
+            "name": "Rice Krispie Treat Bar",
+            "station": "Desearts"
+        }
+    ]
+};
+
+
+export { ucsbDiningCommonsMenuItemFixtures };
