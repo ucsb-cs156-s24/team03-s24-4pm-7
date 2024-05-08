775c5dcc3e9c12dfb17693c395576384fb399b0c
Michelle Zimmermann <michellezimmermann@Michelles-MacBook-Pro.local>
2024-05-07 17:51:24 -0700

    adding articleFixtures javascript

---
+++ frontend/src/fixtures/articlesFixtures.js
@@ -0,0 +1,36 @@
+const articlesFixtures = {
+    oneArticle: {
+        "id" : 1,
+        "title" : "Google",
+        "url" : "google.com",
+        "explanation" : "this is google search engine",
+        "email" : "g@gmail.com",
+        "localDateTime": "2022-01-02T12:00:00"
+    }, 
+    threeArticles: [
+        {
+            "id" : 2,
+            "title" : "CNN",
+            "url" : "cnn.com",
+            "explanation" : "this is CNN's homepage",
+            "email" : "c@cnn.com",
+            "localDateTime": "2022-01-02T12:00:00"
+        }, {
+            "id" : 3,
+            "title" : "BBC",
+            "url" : "bbc.com",
+            "explanation" : "this is BBC's homepage",
+            "email" : "b@bbc.com",
+            "localDateTime": "2022-04-02T12:00:00"
+        }, {
+            "id" : 4,
+            "title" : "The New York Times",
+            "url" : "nytimes.com",
+            "explanation" : "this is the New York Times' homepage",
+            "email" : "n@nyt.com",
+            "localDateTime": "2022-08-02T12:00:00"
+        }, 
+    ]
+};
+
+export {articlesFixtures};
