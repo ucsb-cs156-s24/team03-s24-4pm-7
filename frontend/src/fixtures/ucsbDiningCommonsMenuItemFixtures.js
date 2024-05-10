2684f458fe44141435b14e03c2c311c5f5de71ab
chendashi <chendashi111@outlook.com>
2024-05-10 01:04:53 +0000

    zc - fix fixtures

--- frontend/src/fixtures/ucsbDiningCommonsMenuItemFixtures.js
+++ frontend/src/fixtures/ucsbDiningCommonsMenuItemFixtures.js
@@ -1,31 +1,35 @@
 const ucsbDiningCommonsMenuItemFixtures = {
-    oneItem: {
-        "id": 1,
-        "diningCommonsCode": "ortega",
-        "name": "Sesame Beef with Broccoli",
-        "station": "Entree Specials"
-    },
-    threeItems: [
-        {
-            "id": 1,
-            "diningCommonsCode": "ortega",
-            "name": "Kung Pao Chicken",
-            "station": "Entree Specials"
-        },
-        {
-            "id": 2,
-            "diningCommonsCode": "ortega",
-            "name": "Classic Miso Soup",
-            "station": "Entree"
-        },
-        {
-            "id": 3,
-            "diningCommonsCode": "ortega",
-            "name": "Rice Krispie Treat Bar",
-            "station": "Desearts"
-        }
-    ]
-};
+    oneRestaurant:
+        [
+            {
+                "id": 1,
+                "diningCommonsCode": "ortega",
+                "name": "Sesame Beef with Broccoli",
+                "station": "Entree Specials"
+            }
+        ],
 
+    threeRestaurants:
+        [
+            {
+                "id": 2,
+                "diningCommonsCode": "ortega",
+                "name": "Kung Pao Chickeno",
+                "station": "Entree Specials"
+            },
+            {
+                "id": 3,
+                "diningCommonsCode": "ortega",
+                "name": "Classic Miso Soup",
+                "station": "Entree"
+            },
+            {
+                "id": 1,
+                "diningCommonsCode": "ortega",
+                "name": "Rice Krispie Treat Bar",
+                "station": "Desearts"
+            }
+        ]
+};
 
 export { ucsbDiningCommonsMenuItemFixtures };
