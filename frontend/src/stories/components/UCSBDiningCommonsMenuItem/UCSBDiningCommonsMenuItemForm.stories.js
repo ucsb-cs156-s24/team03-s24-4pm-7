0c69c83d80930826998e55f01bc336116faca5d1
chendashi <chendashi111@outlook.com>
2024-05-10 06:26:24 +0000

    zc - another update on storybook for form

--- frontend/src/stories/components/UCSBDiningCommonsMenuItem/UCSBDiningCommonsMenuItemForm.stories.js
+++ frontend/src/stories/components/UCSBDiningCommonsMenuItem/UCSBDiningCommonsMenuItemForm.stories.js
@@ -26,7 +26,7 @@ Create.args = {
 export const Update = Template.bind({});
 
 Update.args = {
-    initialContents: ucsbDiningCommonsMenuItemFixtures.oneMenuItem,
+    initialContents: ucsbDiningCommonsMenuItemFixtures.oneItem,
     buttonLabel: "Update",
     submitAction: (data) => {
         console.log("Submit was clicked with data: ", data); 
