c0eab4a53ca9a83efae375b7f177018b349fe22f
Nadav Avital <nadav.avital@icloud.com>
2024-05-13 13:10:55 -0700

    implemented create page, passes all tests locally

--- frontend/src/main/components/MenuItemReview/MenuItemReviewForm.js
+++ frontend/src/main/components/MenuItemReview/MenuItemReviewForm.js
@@ -51,7 +51,7 @@ function MenuItemReviewForm({ initialContents, submitAction, buttonLabel = "Crea
                     <Form.Group className="mb-3" >
                         <Form.Label htmlFor="itemId">itemId</Form.Label>
                         <Form.Control
-                            data-testid="MenuItemReview-itemId"
+                            data-testid="MenuItemReviewForm-itemId"
                             id="itemId"
                             type="text"
                             isInvalid={Boolean(errors.itemId)}
