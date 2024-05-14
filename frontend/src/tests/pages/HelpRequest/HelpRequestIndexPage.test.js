d17c84c024a1ee5b945d35d42d05f7f5973273e5
Michelle Zimmermann <michellezimmermann@Michelles-MacBook-Pro.local>
2024-05-13 23:29:24 -0700

    all

--- frontend/src/tests/pages/HelpRequest/HelpRequestIndexPage.test.js
+++ frontend/src/tests/pages/HelpRequest/HelpRequestIndexPage.test.js
@@ -1,16 +1,14 @@
-import { fireEvent, render, waitFor, screen } from "@testing-library/react";
+import { render, screen, waitFor, fireEvent } from "@testing-library/react";
+import HelpRequestIndexPage from "main/pages/HelpRequest/HelpRequestIndexPage";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { MemoryRouter } from "react-router-dom";
-import UCSBDatesIndexPage from "main/pages/UCSBDates/UCSBDatesIndexPage";
-
+import mockConsole from "jest-mock-console";
+import { helpRequestFixtures } from "fixtures/helpRequestFixtures";
 
 import { apiCurrentUserFixtures } from "fixtures/currentUserFixtures";
 import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
-import { ucsbDatesFixtures } from "fixtures/ucsbDatesFixtures";
 import axios from "axios";
 import AxiosMockAdapter from "axios-mock-adapter";
-import mockConsole from "jest-mock-console";
-
 
 const mockToast = jest.fn();
 jest.mock('react-toastify', () => {
@@ -22,11 +20,11 @@ jest.mock('react-toastify', () => {
     };
 });
 
-describe("UCSBDatesIndexPage tests", () => {
+describe("HelpRequestIndexPage tests", () => {
 
     const axiosMock = new AxiosMockAdapter(axios);
 
-    const testId = "UCSBDatesTable";
+    const testId = "HelpRequestTable";
 
     const setupUserOnly = () => {
         axiosMock.reset();
@@ -42,117 +40,116 @@ describe("UCSBDatesIndexPage tests", () => {
         axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
     };
 
-    test("Renders with Create Button for admin user", async () => {
-        // arrange
-        setupAdminUser();
-        const queryClient = new QueryClient();
-        axiosMock.onGet("/api/ucsbdates/all").reply(200, []);
 
-        // act
+    const queryClient = new QueryClient();
+
+    test("Renders with Create Button for admin user", async () => {
+        setupAdminUser();
+        axiosMock.onGet("/api/helprequest/all").reply(200, []);
+
         render(
             <QueryClientProvider client={queryClient}>
                 <MemoryRouter>
-                    <UCSBDatesIndexPage />
+                    <HelpRequestIndexPage />
                 </MemoryRouter>
             </QueryClientProvider>
         );
 
-        // assert
-        await waitFor( ()=>{
-            expect(screen.getByText(/Create UCSBDate/)).toBeInTheDocument();
+        await waitFor(() => {
+            expect(screen.getByText(/Create HelpRequest/)).toBeInTheDocument();
         });
-        const button = screen.getByText(/Create UCSBDate/);
-        expect(button).toHaveAttribute("href", "/ucsbdates/create");
+        const button = screen.getByText(/Create HelpRequest/);
+        expect(button).toHaveAttribute("href", "/helprequest/create");
         expect(button).toHaveAttribute("style", "float: right;");
     });
 
-    test("renders three dates correctly for regular user", async () => {
-        
-        // arrange
+    test("renders three HelpRequests correctly for regular user", async () => {
         setupUserOnly();
-        const queryClient = new QueryClient();
-        axiosMock.onGet("/api/ucsbdates/all").reply(200, ucsbDatesFixtures.threeDates);
+        axiosMock.onGet("/api/helprequest/all").reply(200, helpRequestFixtures.threeHelpRequests);
 
-        // act
         render(
             <QueryClientProvider client={queryClient}>
                 <MemoryRouter>
-                    <UCSBDatesIndexPage />
+                    <HelpRequestIndexPage />
                 </MemoryRouter>
             </QueryClientProvider>
         );
 
-        // assert
         await waitFor(() => { expect(screen.getByTestId(`${testId}-cell-row-0-col-id`)).toHaveTextContent("1"); });
         expect(screen.getByTestId(`${testId}-cell-row-1-col-id`)).toHaveTextContent("2");
         expect(screen.getByTestId(`${testId}-cell-row-2-col-id`)).toHaveTextContent("3");
 
-        // assert that the Create button is not present when user isn't an admin
-        expect(screen.queryByText(/Create UCSBDate/)).not.toBeInTheDocument();
+        const createHelpRequestButton = screen.queryByText("Create HelpRequest");
+        expect(createHelpRequestButton).not.toBeInTheDocument();
 
+        expect(screen.getByText("email1@gmail.com")).toBeInTheDocument();
+        expect(screen.getByText("01")).toBeInTheDocument();
+        expect(screen.getByText("breakout")).toBeInTheDocument();
+        expect(screen.getByText("2022-01-02T12:00:00")).toBeInTheDocument();
+        expect(screen.getByText("sample explanation for team01")).toBeInTheDocument();
+        expect(screen.getByText("true")).toBeInTheDocument();
+
+        // for non-admin users, details button is visible, but the edit and delete buttons should not be visible
+        expect(screen.queryByTestId("HelpRequestTable-cell-row-0-col-Delete-button")).not.toBeInTheDocument();
+        expect(screen.queryByTestId("HelpRequestTable-cell-row-0-col-Edit-button")).not.toBeInTheDocument();
     });
 
-
     test("renders empty table when backend unavailable, user only", async () => {
-        // arrange
         setupUserOnly();
-        const queryClient = new QueryClient();
-        axiosMock.onGet("/api/ucsbdates/all").timeout();
+
+        axiosMock.onGet("/api/helprequest/all").timeout();
+
         const restoreConsole = mockConsole();
 
-        // act
         render(
             <QueryClientProvider client={queryClient}>
                 <MemoryRouter>
-                    <UCSBDatesIndexPage />
+                    <HelpRequestIndexPage />
                 </MemoryRouter>
             </QueryClientProvider>
         );
 
-        // assert
         await waitFor(() => { expect(axiosMock.history.get.length).toBeGreaterThanOrEqual(1); });
-
+        
         const errorMessage = console.error.mock.calls[0][0];
-        expect(errorMessage).toMatch("Error communicating with backend via GET on /api/ucsbdates/all");
+        expect(errorMessage).toMatch("Error communicating with backend via GET on /api/helprequest/all");
         restoreConsole();
 
-        expect(screen.queryByTestId(`${testId}-cell-row-0-col-id`)).not.toBeInTheDocument();
     });
 
     test("what happens when you click delete, admin", async () => {
-        // arrange
         setupAdminUser();
-        const queryClient = new QueryClient();
-        axiosMock.onGet("/api/ucsbdates/all").reply(200, ucsbDatesFixtures.threeDates);
-        axiosMock.onDelete("/api/ucsbdates").reply(200, "UCSBDate with id 1 was deleted");
 
-        // act
+        axiosMock.onGet("/api/helprequest/all").reply(200, helpRequestFixtures.threeHelpRequests);
+        axiosMock.onDelete("/api/helprequest").reply(200, "HelpRequest with id 1 was deleted");
+
+
         render(
             <QueryClientProvider client={queryClient}>
                 <MemoryRouter>
-                    <UCSBDatesIndexPage />
+                    <HelpRequestIndexPage />
                 </MemoryRouter>
             </QueryClientProvider>
         );
 
-        // assert
         await waitFor(() => { expect(screen.getByTestId(`${testId}-cell-row-0-col-id`)).toBeInTheDocument(); });
 
         expect(screen.getByTestId(`${testId}-cell-row-0-col-id`)).toHaveTextContent("1");
 
+
         const deleteButton = screen.getByTestId(`${testId}-cell-row-0-col-Delete-button`);
         expect(deleteButton).toBeInTheDocument();
 
-        // act
         fireEvent.click(deleteButton);
 
-        // assert
-        await waitFor(() => { expect(mockToast).toBeCalledWith("UCSBDate with id 1 was deleted") });
+        await waitFor(() => { expect(mockToast).toBeCalledWith("HelpRequest with id 1 was deleted") });
 
+        await waitFor(() => { expect(axiosMock.history.delete.length).toBe(1); });
+        expect(axiosMock.history.delete[0].url).toBe("/api/helprequest");
+        expect(axiosMock.history.delete[0].url).toBe("/api/helprequest");
+        expect(axiosMock.history.delete[0].params).toEqual({ id: 1 });
     });
 
 });
-
-
 
 
