9026a7cfafd2319e219d0b122f50c36e4129ffac
Michelle Zimmermann <michellezimmermann@Michelles-MacBook-Pro.local>
2024-05-14 13:20:51 -0700

    fixing edit adding another entry

--- frontend/src/main/pages/Articles/ArticlesEditPage.js
+++ frontend/src/main/pages/Articles/ArticlesEditPage.js
@@ -1,72 +1,143 @@
+// import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+// import { useParams } from "react-router-dom";
+// import ArticlesForm from "main/components/Articles/ArticlesForm";
+// import { Navigate } from 'react-router-dom'
+// import { useBackend, useBackendMutation } from "main/utils/useBackend";
+// import { toast } from "react-toastify";
+
+// export default function ArticlesEditPage({storybook=false}) {
+//   let { id } = useParams();
+
+//   const { data: article, _error, _status } =
+//     useBackend(
+//       // Stryker disable next-line all : don't test internal caching of React Query
+//       [`/api/articles?id=${id}`],
+//       {  // Stryker disable next-line all : GET is the default, so changing this to "" doesn't introduce a bug
+//         method: "GET",
+//         url: `/api/articles`,
+//         params: {
+//           id
+//         }
+//       }
+//     );
+
+
+//   const objectToAxiosPutParams = (article) => ({
+//     url: "/api/articles",
+//     method: "PUT",
+//     params: {
+//       id: article.id,
+//     },
+//     data: {
+//       title: article.title,
+//       url: article.url,
+//       explanation: article.explanation,
+//       email: article.email,
+//       localDateTime: article.localDateTime
+//     }
+//   });
+
+//   const onSuccess = (article) => {
+//     toast(`Article Updated - id: ${article.id} title: ${article.title}`);
+//   }
+
+//   const mutation = useBackendMutation(
+//     objectToAxiosPutParams,
+//     { onSuccess },
+//     // Stryker disable next-line all : hard to set up test for caching
+//     [`/api/articles?id=${id}`]
+//   );
+
+//   const { isSuccess } = mutation
+
+//   const onSubmit = async (data) => {
+//     mutation.mutate(data);
+//   }
+
+//   if (isSuccess && !storybook) {
+//     return <Navigate to="/articles" />
+//   }
+
+//   return (
+//     <BasicLayout>
+//       <div className="pt-2">
+//         <h1>Edit Article</h1>
+//         {
+//           article && <ArticlesForm submitAction={onSubmit} buttonLabel="Update" initialContents={article}/>
+//         }
+//       </div>
+//     </BasicLayout>
+//   )
+// }
+
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
 import { useParams } from "react-router-dom";
-import ArticlesForm from "main/components/Articles/ArticlesForm";
+import ArticlesForm from 'main/components/Articles/ArticlesForm';
 import { Navigate } from 'react-router-dom'
 import { useBackend, useBackendMutation } from "main/utils/useBackend";
 import { toast } from "react-toastify";
 
 export default function ArticlesEditPage({storybook=false}) {
-  let { id } = useParams();
+    let { id } = useParams();
 
-  const { data: article, _error, _status } =
-    useBackend(
-      // Stryker disable next-line all : don't test internal caching of React Query
-      [`/api/articles?id=${id}`],
-      {  // Stryker disable next-line all : GET is the default, so changing this to "" doesn't introduce a bug
-        method: "GET",
-        url: `/api/articles`,
+    const { data: articles, _error, _status } =
+        useBackend(
+            // Stryker disable next-line all : don't test internal caching of React Query
+            [`/api/articles?id=${id}`],
+            {  // Stryker disable next-line all : GET is the default, so mutating this to "" doesn't introduce a bug
+                method: "GET",
+                url: `/api/articles`,
+                params: {
+                    id
+                }
+            }
+        );
+
+    const objectToAxiosPutParams = (articles) => ({
+        url: "/api/articles",
+        method: "PUT",
         params: {
-          id
+            id: articles.id,
+        },
+        data: {
+            title: articles.title,
+            url: articles.url,
+            explanation: articles.explanation,
+            email: articles.email,
+            dateAdded: articles.dateAdded,
         }
-      }
+    });
+
+    const onSuccess = (articles) => {
+        toast(`Article Updated - id: ${articles.id} title: ${articles.title}`);
+    }
+
+    const mutation = useBackendMutation(
+        objectToAxiosPutParams,
+        { onSuccess },
+        // Stryker disable next-line all : hard to set up test for caching
+        [`/api/articles?id=${id}`]
     );
 
+    const { isSuccess } = mutation
 
-  const objectToAxiosPutParams = (article) => ({
-    url: "/api/articles",
-    method: "PUT",
-    params: {
-      id: article.id,
-    },
-    data: {
-      title: article.title,
-      url: article.url,
-      explanation: article.explanation,
-      email: article.email,
-      localDateTime: article.localDateTime
+    const onSubmit = async (data) => {
+        mutation.mutate(data);
     }
-  });
 
-  const onSuccess = (article) => {
-    toast(`Article Updated - id: ${article.id} title: ${article.title}`);
-  }
+    if (isSuccess && !storybook) {
+        return <Navigate to="/articles" />
+    }
 
-  const mutation = useBackendMutation(
-    objectToAxiosPutParams,
-    { onSuccess },
-    // Stryker disable next-line all : hard to set up test for caching
-    [`/api/articles?id=${id}`]
-  );
+    return (
+        <BasicLayout>
+            <div className="pt-2">
+                <h1>Edit Article</h1>
+                {
+                    articles && <ArticlesForm submitAction={onSubmit} buttonLabel={"Update"} initialContents={articles} />
+                }
+            </div>
+        </BasicLayout>
+    )
 
-  const { isSuccess } = mutation
-
-  const onSubmit = async (data) => {
-    mutation.mutate(data);
-  }
-
-  if (isSuccess && !storybook) {
-    return <Navigate to="/articles" />
-  }
-
-  return (
-    <BasicLayout>
-      <div className="pt-2">
-        <h1>Edit Article</h1>
-        {
-          article && <ArticlesForm initialContents={article} submitAction={onSubmit} buttonLabel="Update" />
-        }
-      </div>
-    </BasicLayout>
-  )
 }
-
